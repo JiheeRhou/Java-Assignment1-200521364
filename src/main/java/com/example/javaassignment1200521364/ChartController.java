a98588d695210b72029afb09875b48cee08509c8
JiheeRhou <200521364@student.georgianc.on.ca>
2023-06-01 13:40:31 -0400

    first commit

---
+++ src/main/java/com/example/javaassignment1200521364/ChartController.java
@@ -0,0 +1,4 @@
+package com.example.javaassignment1200521364;
+
+public class ChartController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javaassignment1200521364 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javaassignment1200521364 to javafx.fxml;
+    exports com.example.javaassignment1200521364;
+}
